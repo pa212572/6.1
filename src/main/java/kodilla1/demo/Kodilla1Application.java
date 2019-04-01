d9a0f1345d4d1d447d2407706048f3fea1eb295b
Jakub Podolski <jakub.podolski2@gmail.com>
2019-04-01 15:24:33 +0200

    first commit

---
+++ src/main/java/kodilla1/demo/Kodilla1Application.java
@@ -0,0 +1,13 @@
+package kodilla1.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kodilla1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kodilla1Application.class, args);
+    }
+
+}
---
+++ src/test/java/kodilla1/demo/Kodilla1ApplicationTests.java
@@ -0,0 +1,16 @@
+package kodilla1.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Kodilla1ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
